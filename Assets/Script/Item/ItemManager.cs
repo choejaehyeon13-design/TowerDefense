77944b0f647cbe9e1b23012b4623029c0c077e45
MIN <minbh060807@gmail.com>
2026-05-13 12:32:02 +0900

    give upgrade

--- Assets/Script/Item/ItemManager.cs
+++ Assets/Script/Item/ItemManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ItemManager : MonoBehaviour
 {
@@ -13,6 +14,7 @@ public class ItemManager : MonoBehaviour
     public float TimeSlowLast = 3f;
     public float TeamBuffLast = 2f;
     public int giveUpgradeCost = 10;
+    public Text GiveText;
     void Awake()
     {
         Instance = this;
@@ -21,7 +23,7 @@ public class ItemManager : MonoBehaviour
     void Start() 
     {
         StartCoroutine(GiveItemLoop());
-
+        UpdateGiveText();
     }
     void Update() //스페이스로 아이템 사용
     {
@@ -29,7 +31,12 @@ public class ItemManager : MonoBehaviour
         {
             UseItem();
         }
+
         
+    }
+    void UpdateGiveText()
+    {
+        GiveText.text = $"지급 주기 : {giveInterval}초 (업그레이드 비용: {giveUpgradeCost} 골드)";
     }
     void UseItem() //아이템 사용
     {
@@ -166,5 +173,7 @@ public class ItemManager : MonoBehaviour
         else {
             Debug.Log("최대 업그레이드");
         }
+        UpdateGiveText();
+        
     }
 }
